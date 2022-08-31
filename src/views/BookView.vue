cd5e2a443cf69c8ee4456cebedbf62a65af4f531
francis-njoku <njokuchimauche@gmail.com>
2022-08-31 15:33:12 +0100

    Fix bug on comment

--- src/views/BookView.vue
+++ src/views/BookView.vue
@@ -2,47 +2,48 @@
   <div class="py-5 px-8">
     <div v-if="loading" class="flex justify-center">Loading...</div>
     <div v-else class="container mx-auto">
-      <div class="grid grid-cols-6 items-center">
-        <div class="mr-4">
-          <!-- <img :src="survey.image_url" :alt="survey.title" />-->
+      <form @submit.prevent="saveBook">
+        <div class="grid grid-cols-6 items-center">
+          <div class="mr-4">
+            <!-- <img :src="survey.image_url" :alt="survey.title" />-->
+          </div>
+          <div class="col-span-5">
+            <h1 class="text-3xl mb-3">Name of Book: {{ book.book.name }}</h1>
+            <p class="text-gray-500 text-sm" v-html="book.book.authors"></p>
+            <p class="text-gray-500 text-sm">
+              Number of pages: {{ book.book.numberOfPages }}
+            </p>
+          </div>
         </div>
-        <div class="col-span-5">
-          <h1 class="text-3xl mb-3">Name of Book: {{ book.book.name }}</h1>
-          <p class="text-gray-500 text-sm" v-html="book.book.authors"></p>
-          <p class="text-gray-500 text-sm">
-            Number of pages: {{ book.book.numberOfPages }}
-          </p>
-        </div>
-      </div>
-      <div
-        v-if="bookFinished"
-        class="py-8 px-6 bg-emerald-400 text-white w-[600px] mx-auto"
-      >
-        <div class="text-xl mb-3 font-semibold">Comment has been sent.</div>
-        <button
-          @click="submitAnotherComment"
-          type="button"
-          class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        <div
+          v-if="bookFinished"
+          class="py-8 px-6 bg-emerald-400 text-white w-[600px] mx-auto"
         >
-          Submit another comment
-        </button>
-      </div>
-      <div v-else>
-        <hr class="my-3" />
-        <h3>Comments</h3>
-        <div v-for="(comments, ind) of book.comment" :key="comments.postid">
-          <p>{{ comments.comment }}</p>
-          <hr />
-          <br /><br />
+          <div class="text-xl mb-3 font-semibold">Comment has been sent.</div>
+          <button
+            @click="submitAnotherComment"
+            type="button"
+            class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Submit another comment
+          </button>
         </div>
-        <!--<div v-for="(question, ind) of survey.questions" :key="question.id">
+        <div v-else>
+          <hr class="my-3" />
+          <h3>Comments</h3>
+          <div v-for="(comments, ind) of book.comment" :key="comments.postid">
+            <p>{{ comments.comment }}</p>
+            <hr />
+            <br /><br />
+          </div>
+          <!--<div v-for="(question, ind) of survey.questions" :key="question.id">
           <QuestionViewer
             v-model="answers[question.id]"
             :question="question"
             :index="ind"
           />
         </div>-->
-        <form @submit.prevent="submitBook">
+
           <!---<input type="hidden" v-model="model.status" :value="'active'" />-->
 
           <label
@@ -65,8 +66,8 @@
           >
             Submit
           </button>
-        </form>
-      </div>
+        </div>
+      </form>
     </div>
   </div>
 </template>
@@ -93,6 +94,8 @@ let model = ref({
 });
 const answers = ref({});
 
+// Watch to current survey data change and when this happens we update local model
+
 store.dispatch("getBook", route.params.id);
 
 function submitBook() {
@@ -114,21 +117,23 @@ function submitBook() {
     });
 }
 
-function saveSurvey() {
+function saveBook() {
   //let data = "chima";
   console.log(model.value);
 
   store
-    .dispatch("saveComment", model.value)
+    .dispatch("saveComment", {
+      bookId: book.value.id,
+      comment: model.value.comment,
+      postid: duel,
+      status: "active",
+    })
     .then(({ data }) => {
       store.commit("notify", {
         type: "success",
-        message: "Survey was successfully updated",
+        message: "Comment was successfully added",
       });
-      router.push({
-        name: "SurveyView",
-        params: { id: data.data.id },
-      });
+      router.go();
     })
     .catch((error) => {
       console.log(error.response);
